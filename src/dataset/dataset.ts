53c1a1ae7e66a00acead430da2b436849020de82
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-11-13 14:10:20 -0500

    create dataset

---
+++ src/dataset/dataset.ts
@@ -0,0 +1,30 @@
+import { isEmpty } from "lodash";
+import { BigQuery } from "@google-cloud/bigquery";
+import { log } from "../logs";
+
+/**
+ * 
+ * @param datasetId 
+ * @param location - e.g "US" Specify the geographic location where the dataset should reside
+ * @param options 
+ */
+export const createDataset = async (datasetId = 'my_new_dataset', location: string = "US", options?: any): Promise<any | null> => {
+
+    const bigquery = new BigQuery();
+
+    try {
+
+      const options = {
+        location,
+      };
+  
+      // Create a new dataset
+      const [dataset] = await bigquery.createDataset(datasetId, options);
+      log(`Dataset ${dataset.id} created.`);
+      return dataset;
+    }
+    catch(error){
+        log('error creating the dataset', error);
+        return null;
+    }
+  }
